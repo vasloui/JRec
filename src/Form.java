f94c0b3b2a8ba6af26900c9dcfc1cfdd31b7a60e
Vassilis <114075186+vasloui@users.noreply.github.com>
2024-02-13 19:17:45 +0200

    Now UI shows as before.

--- src/Form.java
+++ src/Form.java
@@ -8,72 +8,59 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Form implements ActionListener {
+public class Form {
 
 
     private JFrame frame;
     private Recorder recorder;
     private AudioViz viz;
-
-//    Form(){
+    private FormDesigner formDesigner;
+//    Form() {
+//        frame = new JFrame();
+//        // the clickable button
+//        JButton button = new JButton("Click Me");
+//
+//        // Mic audio visualization graph
 //        viz = new AudioViz();
-////        formDesigner = new FormDesigner(viz.scope.getView());
-//        frame = new JFrame("FormDesigner");
-//        frame.setContentPane(panel1);
+//
+//        // Instantiate recorder object
+//        recorder = new Recorder();
+//
+//        // the panel with the button and text
+//        JPanel panel = new JPanel();
+//        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+//        panel.setLayout(new GridLayout(0, 1));
+//        panel.add(button);
+//        panel.add(viz.scope.getView());
+//        //panel.add(label);
+//
+//        // set up the frame and display it
+//        frame.add(panel, BorderLayout.CENTER);
 //        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setTitle("GUI");
 //        frame.pack();
 //        frame.setVisible(true);
-//        this.scope = viz.scope.getView();
 //    }
 
-    Form() {
-        frame = new JFrame();
-        // the clickable button
-        JButton button = new JButton("Click Me");
-        button.addActionListener(this);
+    Form(){
 
-        // Mic audio visualization graph
         viz = new AudioViz();
-
-        // Instantiate recorder object
-        recorder = new Recorder();
-
-        // the panel with the button and text
-        JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        panel.setLayout(new GridLayout(0, 1));
-        panel.add(button);
-        panel.add(viz.scope.getView());
-        //panel.add(label);
-
-        // set up the frame and display it
-        frame.add(panel, BorderLayout.CENTER);
+        frame = new JFrame();
+        formDesigner = new FormDesigner(){
+            @Override
+            public void createUIComponents() {
+                audioView = viz.scope.getView();
+            }
+        };
+        frame.setContentPane(formDesigner.panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("GUI");
         frame.pack();
         frame.setVisible(true);
     }
-
 
     Form(String title){
         this();
         frame.setTitle(title);
     }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        recorder.rec(viz.synth);
-        viz = new AudioViz();
-
-    }
-
-//    @Override
-//    public void createUIComponents(){
-//    }
-
-//    @Override
-//    public void actionPerformed(ActionEvent e) {
-//        recorder.rec(audioViz.synth);
-//    }
 
 }
---
+++ src/FormDesigner.java
@@ -0,0 +1,19 @@
+package src;
+
+import com.jsyn.scope.swing.AudioScopeView;
+
+import javax.swing.*;
+
+public class FormDesigner {
+    public JPanel panel1;
+    private JButton button1;
+    private JSlider slider1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    public AudioScopeView audioView;
+
+    public void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
